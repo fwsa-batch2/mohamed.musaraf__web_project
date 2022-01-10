43ab045ac0314afb930d916ca5f407eaebf216e8
Mohamed Hasan Musaraf <hasanmusaraf786@gmail.com>
2022-01-10 12:52:25 +0530

    js modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,25 +43,7 @@ function searchPages() {
 
     if ('movies' == searchBar) {
         window.open('/pages/index.html')
-    } else if ('stream' == searchBar ) {
-        window.open('/pages/stream.html')
-    } else if ('events' == searchBar) {
-        window.open('/pages/events.html')
-    }else if ('plays' == searchBar) {
-        window.open('/pages/plays.html')
-    }else if ('sports' == searchBar ) {
-        window.open('/pages/sports.html')
-    }else if ('activity' == searchBar) {
-        window.open('/pages/activities.html')
-    }else if ('buzz' == searchBar) {
-        window.open('/pages/buzz.html')
-    }else if ('corporates' == searchBar) {
-        window.open('/pages/corporates.html')
-    }else if ('offers' == searchBar) {
-        window.open('/pages/offers.html')
-    }else if ('gift' == searchBar ) {
-        window.open('/pages/giftCards.html')
-    }else if ('spiderman' == searchBar) {
+    } else if ('spiderman' == searchBar) {
         window.open('/pages/spider.html')
     } else if ('sooriyavanshi' == searchBar) {
         window.open('/pages/sooriyavanshi.html')
@@ -69,26 +51,16 @@ function searchPages() {
         window.open('/pages/residentEvil.html')
     }else if ('pushpa' == searchBar) {
         window.open('/pages/pushpa.html')
-    }else if ('murungakkai' == searchBar) {
-        window.open('/pages/murungakkai.html')
-    }else if ('muddy' == searchBar) {
-        window.open('/pages/muddy.html')
-    }else if ('maraikkayar' == searchBar) {
-        window.open('/pages/maraikkayar.html')
     }else if ('maanadu' == searchBar) {
         window.open('/pages/maanadu.html')
     }else if ('kurup' == searchBar) {
         window.open('/pages/kurup.html')
     }else if ('jail' == searchBar) {
         window.open('/pages/jail.html')
-    }else if ('ikk' == searchBar) {
-        window.open('/pages/ikk.html')
     }else if ('eternals' == searchBar) {
         window.open('/pages/eternals.html')
     }else if ('encanto' == searchBar) {
         window.open('/pages/encanto.html')
-    }else if ('dune' == searchBar) {
-        window.open('/pages/dune.html')
     }else if ('clifford' == searchBar) {
         window.open('/pages/clifford.html')
     }else if ('chandigarh' == searchBar) {
